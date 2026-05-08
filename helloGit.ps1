162e6c3988ada3e0706325080cdc28e3ff909d97
Jules TODJINOU <jtodjinou@datatechnologies.bj>
2026-05-08 15:11:12 +0100

    (c) info added.

--- helloGit.ps1
+++ helloGit.ps1
@@ -1,4 +1,11 @@
-﻿param(
+﻿# Copyright (c) 2026 Jules MAHOUNOU
+# Project  : TRISH
+# Initiated: 17/04/2026
+# Origin   : Benin
+# Contact  : jtodjinou@datatechnologies.bj | +229 0159521211
+# License  : MIT — see LICENSE file for details
+
+param(
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]$PluginArgs
 )
